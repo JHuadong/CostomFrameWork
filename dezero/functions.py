40d8affc4dce46b923308b25faf130631389c775
SamFisher <547958322@qq.com>
2026-07-26 23:46:57 +0800

    step 34: Implement the sine function and verify that it can be differentiated to higher orders.

--- dezero/functions.py
+++ dezero/functions.py
@@ -12,3 +12,30 @@ class Sin(Function):
 
 def sin(x):
     return Sin()(x)
+
+class Cos(Function):
+    def forward(self, x):
+        y = np.cos(x)
+        return y
+
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy * -sin(x)
+        return gx
+
+def cos(x):
+    return Cos()(x)
+
+if __name__ == "__main__":
+    import numpy as np
+    from dezero import Variable
+    import dezero.functions as F
+
+    x = Variable(np.array(1.0))
+    y = F.sin(x)
+    y.backward(create_graph=True)
+    for i in range(3):
+        gx = x.grad
+        x.cleargrad()
+        gx.backward(create_graph=True)
+        print(x.grad)  # n阶导数
