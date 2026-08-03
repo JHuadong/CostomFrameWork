0e6d93ea4e7e81e7c018c564c5d94941bc2e2350
SamFisher <547958322@qq.com>
2026-08-03 21:56:54 +0800

    step 40 - 41: Implemented automatic propagation of arithmetic operations and forward and backward propagation of matrix operations in the Dezero framework.

--- dezero/core.py
+++ dezero/core.py
@@ -152,6 +152,9 @@ class Div(Function):
         x0, x1 = self.inputs
         gx0 = gy / x1
         gx1 = gy * (-x0 / x1 ** 2)
+        if x0.shape != x1.shape:
+            gx0 = dezero.functions.sum_to(gx0, x0.shape)
+            gx1 = dezero.functions.sum_to(gx1, x1.shape)
         return gx0, gx1
 
 class Pow(Function):
@@ -170,11 +173,16 @@ class Pow(Function):
 
 class Add(Function):
     def forward(self, x0, x1):
+        self.x0_shape, self.x1_shape = x0.shape, x1.shape
         y = x0 + x1
         return y
 
     def backward(self, gy):
-        return gy, gy
+        gx0, gx1 = gy, gy
+        if self.x0_shape != self.x1_shape:  # for broadcast
+            gx0 = dezero.functions.sum_to(gx0, self.x0_shape)
+            gx1 = dezero.functions.sum_to(gx1, self.x1_shape)
+        return gx0, gx1
 
 
 class Mul(Function):
@@ -184,7 +192,12 @@ class Mul(Function):
 
     def backward(self, gy):
         x0, x1 = self.inputs
-        return gy * x1, gy * x0
+        gx0 = gy * x1
+        gx1 = gy * x0
+        if x0.shape != x1.shape:    # for broadcast
+            gx0 = dezero.functions.sum_to(gx0, x0.shape)
+            gx1 = dezero.functions.sum_to(gx1, x1.shape)
+        return gx0, gx1
 
 class Neg(Function):
     def forward(self, x):
@@ -195,11 +208,17 @@ class Neg(Function):
 
 class Sub(Function):
     def forward(self, x0, x1):
+        self.x0_shape, self.x1_shape = x0.shape, x1.shape
         y = x0 - x1
         return y
 
     def backward(self, gy):
-        return gy, -gy
+        gx0 = gy
+        gx1 = -gy
+        if self.x0_shape != self.x1_shape:
+            gx0 = dezero.functions.sum_to(gx0, self.x0_shape)
+            gx1 = dezero.functions.sum_to(gx1, self.x1_shape)
+        return gx0, gx1
 
 def pow(x, c):
     return Pow(c)(x)
@@ -252,6 +271,9 @@ def setup_variable():
     Variable.__truediv__ = div
     Variable.__rtruediv__ = rdiv
     Variable.__pow__ = pow
+
+    Variable.matmul = dezero.functions.matmul
+    Variable.dot = dezero.functions.matmul
 
 if __name__ == "__main__":
     x = Variable(np.random.randn(1, 2, 3))
---
+++ steps/step41.py
@@ -0,0 +1,11 @@
+from dezero import Variable
+import dezero.functions as F
+import numpy as np
+
+if __name__ == "__main__":
+    x = Variable(np.random.randn(2, 3))
+    W = Variable(np.random.randn(3, 4))
+    y = F.matmul(x, W)
+    y.backward()
+    print(x.grad.shape)
+    print(W.grad.shape)
