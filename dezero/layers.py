c3a6e3629674493717306628559a2d3fc9f6f802
SamFisher <547958322@qq.com>
2026-08-04 23:28:26 +0800

    step 45: Implementing the aggregation layers、the MLP(Multi-Layer Perceptron) class and the Model class.

--- dezero/layers.py
+++ dezero/layers.py
@@ -9,7 +9,7 @@ class Layer:
         self._params = set()
 
     def __setattr__(self, name, value):
-        if isinstance(value, Parameter):
+        if isinstance(value, (Parameter, Layer)):
             self._params.add(name)
         super().__setattr__(name, value)
 
@@ -26,7 +26,12 @@ class Layer:
 
     def params(self):
         for name in self._params:
-            yield self.__dict__[name]
+            obj = self.__dict__[name]
+
+            if isinstance(obj, Layer):
+                yield from obj.params()
+            else:
+                yield obj
 
     def cleargrads(self):
         for param in self.params():
@@ -54,7 +59,7 @@ class Linear(Layer):
         if self.W.data is None:
             self.in_size = x.shape[1]
             self._init_W()
-        
+
         y = F.linear(x, self.W, self.b)
         return y
 
@@ -65,12 +70,25 @@ class Linear(Layer):
 
 
 if __name__ == '__main__':
-    layer = Layer()
-    layer.p1 = Parameter(np.array(1))
-    layer.p2 = Parameter(np.array(2))
-    layer.p3 = Variable(np.array(3))
-    layer.p4 = 'test'
-    print(layer._params)
-    print('-------------')
-    for name in layer._params:
-        print(name, layer.__dict__[name])
+    import dezero.layers as L
+    import dezero.functions as F
+    from dezero import Layer
+
+    model = Layer()
+    model.l1 = L.Linear(5)  # 只指定输出大小
+    model.l2 = L.Linear(3)
+
+
+    # 进行推理的函数
+    def predict(model, x):
+        y = model.l1(x)
+        y = F.sigmoid(y)
+        y = model.l2(y)
+        return y
+
+
+    # 访问所有参数
+    for p in model.params():
+        print(p)
+    # 重置所有参数的梯度
+    model.cleargrads()
