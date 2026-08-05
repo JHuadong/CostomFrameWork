8543613f35d098ced63e6b292751a8a5884e65c1
SamFisher <547958322@qq.com>
2026-08-05 22:23:57 +0800

    step 46-47: Implement the Optimizer class, the multi-class softmax function and its corresponding loss function.

---
+++ steps/step47.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from dezero import Variable, as_variable
+import dezero.functions as F
+from dezero.models import MLP
+
+
+def softmax1d(x):
+    x = as_variable(x)
+    y = F.exp(x)
+    sum_y = F.sum(y)
+    return y / sum_y
+
+if __name__ == '__main__':
+    x = Variable(np.array([[0.2, -0.4]]))
+    model = MLP((10, 3))
+    y = model(x)
+    p = softmax1d(y)
+    print(y)
+    print(p)
+
+    x = np.array([[0.2, -0.4], [0.3, 0.5], [1.3, -3.2], [2.1, 0.3]])
+    t = np.array([2, 0, 1, 0])
+    y = model(x)
+    loss = F.softmax_cross_entropy_simple(y, t)
+    # F.softmax_cross_entropy(y, t)
+    print(loss)
